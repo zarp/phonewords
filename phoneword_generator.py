412db3ba9206bf8118549512137c102ffe0fef5e
tkacha <tkacha@pop-os.localdomain>
2019-06-09 18:48:29 -0700

    implemented all_wordifications(), updated get_english_vocabulary(), minor doc string changes

--- phoneword_generator.py
+++ phoneword_generator.py
@@ -14,23 +14,19 @@ mapping = {"1": ["1"],
            "0": ["0"]}
 
 def get_english_vocabulary(min_vocab_word_len):
-    """ uses nltk.words word list,  words shorter than min_vocab_word_len will be excluded"""
+    """ Use nltk.words word list,  words shorter than min_vocab_word_len will be excluded."""
     try:
-        vocab = set([word.upper() for word in words.words()])
+        vocab = set([word.upper() for word in words.words() if len(word) >= min_vocab_word_len])
     except:
         print("NLTK 'words' corpus not found. Downloading...")
         import nltk
         nltk.download('words')
-        vocab = set([word.upper() for word in words.words()])
-
-    for el in vocab.copy():
-        if len(el) < min_vocab_word_len:
-            vocab.remove(el)
+        vocab = set([word.upper() for word in words.words() if len(word) >= min_vocab_word_len])
             
     return vocab
 
 def find_valid_word(regexp, vocab, get_all_words = False):
-    """ helper function. Input: regex string. Output: either a single valid English word or a set of all English words matching Input"""
+     """  Output either a single valid English word or a set of all English words matching input regex string. """
     all_words = set()
     for word in vocab: 
         hit = re.match(regexp, word.upper())
@@ -40,12 +36,9 @@ def find_valid_word(regexp, vocab, get_all_words = False):
     return all_words
 
 def number_to_words(num, vocab, mapping, min_word_len = 1):
-    """
-    Input: str representing Phone number. Output: 'wordified' number.
-    If no valid wordifications exist - return original string (sans '-')
-    """
+    """ Convert str representing phone number to wordified str. If no valid wordifications exist - return original digits. """
     def find_usable_substrings(num_str, min_len = 1):
-        """ Helper function. Finds  substrings of num_str and then furter splits them on chars that can't be converted to letters(i.e. '1' and '0')"""   
+        """ Find substr of num_str and furter split them on chars that can't be converted to letters('1' and '0'). """   
         num_len, subs = len(num_str), set()
         
         for i in range(num_len):
@@ -74,12 +67,49 @@ def number_to_words(num, vocab, mapping, min_word_len = 1):
 
 
 def words_to_number(wordified, mapping):
-    """ Reverse of number_to_words() """
+    """ Reverse of number_to_words(). """
     reversed_num = ""
     for ch in wordified.replace("-","").upper():
         reversed_num +=  next(key for key, val in mapping.items() if ch in val)
             
     return reversed_num[0] + '-' + reversed_num[1:4] + "-" + reversed_num[4:7] + "-" + reversed_num[7:]
+
+def all_wordifications(num, vocab, mapping):
+    """ Output all possible combinations of numbers and English words in a phone number. """
+    
+    def gen_partitions(num_str):
+        """ Generate all possible partitions of a string into constituent substrings. """
+        for i in range(len(num_str)):
+            if i == len(num_str) - 1:
+                yield num_str
+
+            first, rest = num_str[0:i+1], num_str[i+1:]
+
+            for j in gen_partitions(rest):
+                yield '~'.join([first, j])
+                
+    num = num.replace("-","")
+
+    subs_dict, ans = dict(), set()
+    for partition in gen_partitions(num):
+        subs = partition.split('~')
+
+        for sub in subs:
+            #adding dict entries for previously not encountered substrings
+            if sub not in subs_dict:
+                if ("1" in sub) or ("0" in sub):
+                    subs_dict[sub] = set()
+                else:
+                    reg = "^" + "".join(["[" + "".join(mapping[ch]) + "]" for ch in sub]) + "$" 
+                    subs_dict[sub] = find_valid_word(reg, vocab, get_all_words = True)
+                subs_dict[sub].add(sub)
+
+        combos = product(*[subs_dict[el] for el in subs])
+        
+        for c in combos:
+            ans.add("".join(c))
+            
+    return ans
 
 if __name__ == '__main__':
 
@@ -92,4 +122,11 @@ if __name__ == '__main__':
     print("\nNUMBER TO WORDS (one possible 'wordification'):")
     test_str = "1-800-724-6837"
     print(test_str,' <-> ', number_to_words(test_str, vocabulary, mapping, min_word_len = 4))
+          
+    print("\nALL WORDIFICAITONS:")
+    test_str = "1-800-724-6837"
+    all_words = all_wordifications(test_str, vocabulary, mapping)
+    print("Done! Printing output (if minimal word size was set to 1 it may take some time):\n")
+    print(all_words)
+
 
